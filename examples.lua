e0d8c12d564c95086e0e919c7ad120ac74d82a59
Dmitry Patrushev <dimaxa7@mail.ru>
2026-05-17 00:57:21 +0300

    quadtree, working example with mouseover

--- examples.lua
+++ examples.lua
@@ -2,14 +2,37 @@
 
 local RANDOM_STRING = 'sgjkjngf325gjnkf'
 
+--[[
+ZO_PostHook(_G, 'ZO_WorldMap_MouseDown', function() d('ZO_WorldMap_MouseDown') end)
+ZO_PostHook(_G, 'ZO_WorldMap_MouseUp', function() d('ZO_WorldMap_MouseUp') end)
+ZO_PostHook(_G, 'ZO_WorldMap_MouseWheel', function() d('ZO_WorldMap_MouseWheel') end)
+ZO_PostHook(_G, 'ZO_WorldMap_MouseEnter', function() d('ZO_WorldMap_MouseEnter') end)
+ZO_PostHook(_G, 'ZO_WorldMap_MouseExit', function() d('ZO_WorldMap_MouseExit') end)
+--]]
+
 local function example1()
-    local map = LibSurfaceTools.Tools.FlexRect(ZO_WorldMapContainer)
+    local function OnMouseEnter(surface)
+        surface[5], surface[6] = 64, 64
+    end
+
+    local function OnMouseExit(surface)
+        surface[5], surface[6] = 32, 32
+    end
+
+    local map = LibSurfaceTools.Tools.FlexRect(ZO_WorldMapContainer, nil, OnMouseEnter, OnMouseExit)
         :SetAnchor(TOPLEFT)
         :SetAnchor(BOTTOMRIGHT)
-        :SetTexture('/esoui/art/tutorial/poi_wayshrine_complete.dds', 2, 2)
+        -- :SetTexture('/esoui/art/tutorial/poi_wayshrine_complete.dds', 2, 2)
+        :SetTexture('/esoui/art/tutorial/poi_wayshrine_complete.dds')
 
-    GLOBAL_MAP = map
+    local control = map.control
 
+    ZO_PostHook(_G, 'ZO_WorldMap_MouseEnter', function(_, ...) control:GetHandler('OnMouseEnter')(control) end)
+    ZO_PostHook(_G, 'ZO_WorldMap_MouseExit', function(_, ...) control:GetHandler('OnMouseExit')(control) end)
+
+    -- GLOBAL_MAP = map
+
+    --[[
     local function inner()
         if #map.surfaces > 1 then return map:Clear() end
 
@@ -25,14 +48,81 @@ local function example1()
 
         map:RemoveSurfacesOfKind(3)
     end
+    --]]
+
+    local function inner()
+        if #map.surfaces > 1 then return map:Clear() end
+
+        local N = 16
+
+        for i = 1, N do
+            local j = 2 * math.pi * (i / N)
+            local x = math.cos(j) * 0.2
+            local y = math.sin(j) * 0.2
+
+            map:Add(0.5 + x, 0.5 - y, 0, 0, 32, 32)
+        end
+
+        -- map:RemoveSurfacesOfKind(3)
+    end
 
     return inner
 end
+
+
+local function example2()
+    local function OnMouseEnter(surface)
+        surface[5], surface[6] = 36, 36
+    end
+
+    local function OnMouseExit(surface)
+        surface[5], surface[6] = 24, 24
+    end
+
+    local map = LibSurfaceTools.Tools.FlexRect(ZO_WorldMapContainer, nil, OnMouseEnter, OnMouseExit)
+        :SetAnchor(TOPLEFT)
+        :SetAnchor(BOTTOMRIGHT)
+        :SetTexture('EsoUI/Art/Miscellaneous/Gamepad/gp_bullet.dds')
+
+    local control = map.control
+
+    ZO_PostHook(_G, 'ZO_WorldMap_MouseEnter', function(_, ...) control:GetHandler('OnMouseEnter')(control) end)
+    ZO_PostHook(_G, 'ZO_WorldMap_MouseExit', function(_, ...) control:GetHandler('OnMouseExit')(control) end)
+
+    local function inner()
+        if #map.surfaces > 1 then GLOBAL_MAP = nil return map:Clear() end
+
+        local cyroCache
+        for _, cache in pairs(Harvest.Data.mapCaches) do
+            if cache.map == 'cyrodiil/ava_whole' then
+                cyroCache = cache
+                break
+            end
+        end
+
+        local zoneId = GetZoneId(GetUnitZoneIndex('player'))
+
+        for i = 1, #cyroCache.worldX do
+            if cyroCache.worldX[i] and cyroCache.worldY[i] then
+                local x, y = cyroCache.worldX[i] * 100, cyroCache.worldY[i] * 100
+                local n_x, n_y = GetNormalizedWorldPosition(zoneId, x, 40000, y)
+
+                map:Add(n_x, n_y, 0, 0, 24, 24)
+            end
+        end
+
+        GLOBAL_MAP = map
+    end
+
+    return inner
+end
+
 
 EVENT_MANAGER:RegisterForEvent(RANDOM_STRING, EVENT_ADD_ONS_LOADED, function()
     EVENT_MANAGER:UnregisterForEvent(RANDOM_STRING, EVENT_ADD_ONS_LOADED)
 
     LibSurfaceToolsExamples = {
         e1 = example1(),
+        e2 = example2(),
     }
 end)
--- main.lua
+++ main.lua
@@ -112,7 +112,7 @@ end
 
 local FlexRect = class()
 
-function FlexRect:__init(parent, name)
+function FlexRect:__init(parent, name, onMouseEnter, onMouseExit)
     parent = parent or addon.control
     name = name or ('$(parent)FlexRect'..parent:GetNumChildren())
 
@@ -126,6 +126,31 @@ function FlexRect:__init(parent, name)
     self.control = control
 
     self.surfaces = {}
+
+    if onMouseEnter then
+        -- df('Mouse over enabled')
+        self.onMouseEnter = onMouseEnter
+        self.onMouseExit = onMouseExit
+        self.mouseOver = {}
+
+        self.hash = {}  -- setmetatable({}, {__mode='k'})
+
+        control:SetMouseEnabled(true)
+
+        control:SetHandler('OnMouseEnter', function()
+            -- df('Mouse over %s', name)
+            EVENT_MANAGER:RegisterForUpdate(EVENT_NAMESPACE..name, 0, function()
+                self:OnUpdate()
+            end)
+        end)
+
+        control:SetHandler('OnMouseExit', function()
+            -- df('Mouse exit %s', name)
+            EVENT_MANAGER:UnregisterForUpdate(EVENT_NAMESPACE..name)
+        end)
+
+        self.quadtree = IMP_QuadTree()
+    end
 
     return self
 end
@@ -155,12 +180,11 @@ function FlexRect:Add(nX, nY, offsetX, offsetY, w, h, atlasX, atlasY)
     offsetX = offsetX or 0
     offsetY = offsetY or 0
 
-    if atlasY == nil then
-        atlasX, atlasY = _atlasIndexToAtlasXY(atlasX, self.atlasSizeX, self.atlasSizeY)
-    end
-
     local tiL, tiR, tiT, tiB = 0, 1, 0, 1
     if self.atlas then
+        if atlasY == nil then
+            atlasX, atlasY = _atlasIndexToAtlasXY(atlasX, self.atlasSizeX, self.atlasSizeY)
+        end
         tiL, tiR, tiT, tiB = _getTextureInsets(atlasX, atlasY, self.atlasSizeX, self.atlasSizeY)
     end
 
@@ -177,15 +201,25 @@ function FlexRect:Add(nX, nY, offsetX, offsetY, w, h, atlasX, atlasY)
     self.control:SetInsets(surfaceNumber, iL, iR, iT, iB)
 
     -- not very safe, but it should work OK until someone used it inproperly
-    self.surfaces[surfaceNumber] = {nX, nY, offsetX, offsetY, w, h, atlasX, atlasY}
+    local surfaceData = {nX, nY, offsetX, offsetY, w, h, atlasX, atlasY}
+    self.surfaces[surfaceNumber] = surfaceData
+
+    if self.quadtree then
+        self.quadtree:Insert(nX, nY, surfaceData)
+        self.hash[surfaceData] = surfaceNumber
+    end
 end
 
 function FlexRect:_updatePositions()
-    local W, H = self.control:GetWidth(), self.control:GetHeight()
-    if self.W == W and self.H == H then return end
+    local control = self.control
+    local surfaces = self.surfaces
 
-    for surfaceIndex = 1, #self.surfaces do
-        local s = self.surfaces[surfaceIndex]
+    local W, H = control:GetWidth(), control:GetHeight()
+    if self.W == W and self.H == H then return end
+    self.W, self.H = W, H
+
+    for surfaceIndex = 1, #surfaces do
+        local s = surfaces[surfaceIndex]
         local nX, nY, offsetX, offsetY, w, h = s[1], s[2], s[3], s[4], s[5], s[6]
 
         local centerX = W * nX + offsetX
@@ -195,17 +229,50 @@ function FlexRect:_updatePositions()
         local iL, iR = centerX - half_w, -W + centerX + half_w
         local iT, iB = centerY - half_h, -H + centerY + half_h
 
-        self.control:SetInsets(surfaceIndex, iL, iR, iT, iB)
+        control:SetInsets(surfaceIndex, iL, iR, iT, iB)
     end
 end
 
+function FlexRect:_updateSurface(surfaceIndex)
+    local control = self.control
+
+    local W, H = control:GetWidth(), control:GetHeight()
+
+    local s = self.surfaces[surfaceIndex]
+    local nX, nY, offsetX, offsetY, w, h = s[1], s[2], s[3], s[4], s[5], s[6]
+
+    local centerX = W * nX + offsetX
+    local centerY = H * nY + offsetY
+
+    local half_w, half_h = w/2, h/2
+    local iL, iR = centerX - half_w, -W + centerX + half_w
+    local iT, iB = centerY - half_h, -H + centerY + half_h
+
+    control:SetInsets(surfaceIndex, iL, iR, iT, iB)
+end
+
 function FlexRect:RemoveSurface(index)
-    table.remove(self.surfaces, index)
+    local surfaces = self.surfaces
+    local surfaceData = surfaces[index]
+
+    table.remove(surfaces, index)
     self.control:RemoveSurface(index)
+
+    if self.quadtree then
+        self.quadtree:Remove(surfaceData)
+
+        -- TODO: rehashing is not the best thing to see... Very expensive deletion
+        local hash = self.hash
+        for i = 1, #surfaces do
+            hash[surfaces[i]] = i
+        end
+    end
 end
 
 function FlexRect:RemoveSurfacesOfKind(atlasX, atlasY)
     -- TODO: kinda slow, make hash table
+
+    if not self.atlas then return end
 
     if atlasY == nil then
         atlasX, atlasY = _atlasIndexToAtlasXY(atlasX, self.atlasSizeX, self.atlasSizeY)
@@ -224,7 +291,78 @@ function FlexRect:Clear()
     self.control:ClearAllSurfaces()
     -- self.control:ClearAnchors()
 
+    -- TODO: clear self.surfaces and self.hash
+
     ZO_ClearNumericallyIndexedTable(self.surfaces)
+
+    if self.quadtree then
+        self.quadtree:Clear()
+    end
+end
+
+function FlexRect:OnUpdate()
+    local x, y = self.control:GetLeft(), self.control:GetTop()
+    local m_x, m_y = GetUIMousePosition()
+
+    -- TODO: width, height optimization
+    local W, H = self.control:GetWidth(), self.control:GetHeight()
+    local mn_x, mn_y = (m_x - x) / W, (m_y - y) / H
+
+    -- TODO: need some arbitrary width and height...
+    -- 100 px, normalize for w and h
+    local n_size = 64 / W
+    local results = self.quadtree:Query(mn_x, mn_y, n_size)
+
+    -- TODO: check if result is different
+
+    -- df('%d', #results)
+
+    local filtered = {}
+    local mr_x, mr_y = m_x - x, m_y - y
+    for _, result in ipairs(results) do
+        -- local surfaceIndex = self.hash[result]
+        local surfaceData = result.data
+        local r_x, r_y = surfaceData[1] * W + surfaceData[3], surfaceData[2] * H + surfaceData[4]
+        local halfSurfaceWidth, halfSurfaceHeight = surfaceData[5] / 2, surfaceData[6] / 2
+
+        if r_x + halfSurfaceWidth > mr_x and
+        r_x - halfSurfaceWidth < mr_x and
+        r_y - halfSurfaceHeight < mr_y and
+        r_y + halfSurfaceHeight > mr_y then
+            filtered[#filtered+1] = self.hash[surfaceData]
+        end
+    end
+
+    -- df('%d', #filtered)
+
+    local previousMouseOver = self.mouseOver
+    local mouseOver = {}
+    self.mouseOver = mouseOver
+
+    local added = {}
+
+    for i = 1, #filtered do
+        local surfaceIndex = filtered[i]
+        local surface = self.surfaces[surfaceIndex]
+
+        mouseOver[surface] = true
+
+        if not previousMouseOver[surface] then
+            added[surface] = true
+        end
+    end
+
+    for surface in pairs(previousMouseOver) do
+        if not mouseOver[surface] then
+            self.onMouseExit(surface)
+            self:_updateSurface(self.hash[surface])
+        end
+    end
+
+    for surface in pairs(added) do
+        self.onMouseEnter(surface)
+        self:_updateSurface(self.hash[surface])
+    end
 end
 
 addon.Tools = {
@@ -242,12 +380,24 @@ end
 
 -- ----------------------------------------------------------------------------
 
+local addons = {}
 local function OnAddonLoaded(_, addonName)
+    local handler = ZO_WorldMapContainer:GetHandler('OnMouseEnter')
+    if handler then
+        addons[#addons+1] = addonName
+    else
+        addons[#addons+1] = '__' .. addonName
+    end
+
     if addonName ~= addon.name then return end
-    EVENT_MANAGER:UnregisterForEvent(EVENT_NAMESPACE, EVENT_ADD_ON_LOADED)
+    -- EVENT_MANAGER:UnregisterForEvent(EVENT_NAMESPACE, EVENT_ADD_ON_LOADED)
 
     addon:Initialize()
 end
 
 
 EVENT_MANAGER:RegisterForEvent(EVENT_NAMESPACE, EVENT_ADD_ON_LOADED, OnAddonLoaded)
+
+EVENT_MANAGER:RegisterForEvent(EVENT_NAMESPACE, EVENT_ADD_ONS_LOADED, function()
+    d(table.concat(addons, ','))
+end)
