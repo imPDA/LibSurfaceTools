29a2945f487920282e66fbfab95bba4925d18d13
Dmitry Patrushev <dimaxa7@mail.ru>
2026-05-25 14:16:47 +0300

    fishing holes example

--- examples.lua
+++ examples.lua
@@ -118,11 +118,98 @@ local function example2()
 end
 
 
+
+local function example3()
+    local map = LibSurfaceTools.Tools.FlexRect(ZO_WorldMapContainer)
+        :SetTexture('LibSurfaceTools/media/fish.dds', 4, 1)
+
+    local filters = {}
+
+    function SetFilter(filterId, isChecked)
+        df('Set filter %d to %s', filterId, tostring(isChecked))
+        -- collectgarbage('stop')
+        if isChecked then
+            if not filters[filterId] then
+                -- public DecodeData from MapPins
+                local data = GLOBAL_DECODE_DATA('FishingNodes', GetCurrentMapId())
+                for i = 1, #data do
+                    local pin = data[i]
+                    if pin[3] == filterId then
+                        map:Add(pin[1], pin[2], 0, 0, 32, 32, pin[3])
+                    end
+                end
+            end
+            filters[filterId] = true
+        else
+            if filters[filterId] then
+                map:RemoveSurfacesOfKind(filterId)
+            end
+            filters[filterId] = false
+        end
+        -- collectgarbage('restart')
+    end
+
+    local function CreateMapFilters()
+        local worldMapFrameControl = ZO_WorldMapMapFrame
+
+        if not worldMapFrameControl then
+            return
+        end
+
+        local panel = worldMapFrameControl:CreateControl('$(parent)MapFilterPanel', CT_CONTROL)
+        panel:SetAnchor(TOPRIGHT, worldMapFrameControl, TOPRIGHT, -20, 20)
+        panel:SetDimensions(80, 120)
+        panel:SetDrawTier(DT_HIGH)
+        panel:SetDrawLayer(DL_OVERLAY)
+
+        local textures   = {
+            "/esoui/art/icons/crafting_slaughterfish.dds",	      -- Foul
+            "/esoui/art/icons/crafting_fishing_river_betty.dds",  -- River
+            "/esoui/art/icons/crafting_fishing_merringar.dds",	  -- Salt
+            "/esoui/art/icons/crafting_fishing_perch.dds"	      -- Lake
+        }
+
+        local lastControl = nil
+        for i = 1, 4 do
+            local cb = CreateControlFromVirtual('$(parent)MapFilterCB', panel, 'ZO_CheckButton', i)
+            -- cb:SetText(labels[i])
+            -- cb:SetDimensions(160, 25)
+            if i == 1 then
+                cb:SetAnchor(TOPLEFT, panel, TOPLEFT, 0, 0)
+            else
+                cb:SetAnchor(TOPLEFT, lastControl, BOTTOMLEFT, 0, 5)
+            end
+            ZO_CheckButton_SetToggleFunction(cb, function(_, checked)
+                SetFilter(i, checked)
+            end)
+
+            local tex = cb:CreateControl(nil, CT_TEXTURE)
+            tex:SetAnchor(LEFT, cb, RIGHT, 5, 0)
+            tex:SetDimensions(24, 24)
+            tex:SetTexture(textures[i])
+
+            lastControl = cb
+        end
+
+        return panel
+    end
+
+    local filtersControl
+    local function inner()
+        filtersControl = filtersControl or CreateMapFilters()
+        GLOBAL_MAP = map
+    end
+
+    return inner
+end
+
+
 EVENT_MANAGER:RegisterForEvent(RANDOM_STRING, EVENT_ADD_ONS_LOADED, function()
     EVENT_MANAGER:UnregisterForEvent(RANDOM_STRING, EVENT_ADD_ONS_LOADED)
 
     LibSurfaceToolsExamples = {
         e1 = example1(),
         e2 = example2(),
+        e3 = example3(),
     }
 end)
